60a6284e904b09f865cf95c367f579fa1bbdd604
Abdull-Rahman <abed472011@gmail.com>
2022-02-08 22:00:38 +0200

    updates

--- utils/fb.js
+++ utils/fb.js
@@ -1,44 +1,49 @@
-const { getDoc, collection, addDoc, updateDoc, doc } = require('firebase/firestore');
+const { getDoc, collection, addDoc, updateDoc, doc, setDoc, getFirestore } = require('firebase/firestore');
 const fbApp = require('./fb_app_init');
 
 exports.getUser = (userId) => {
     return new Promise((resolve, reject) => {
-        let d = doc(fbApp, '/users/' + userId);
+        let fs = getFirestore(fbApp);
+        let d = doc(fs, 'users', userId);
         getDoc(d).then(res => resolve(res.data())).catch(err => reject(err));
     });
 }
 
 exports.updateUser = (userId, data) => {
     return new Promise((resolve, reject) => {
-        let d = doc(fbApp, '/users/' + userId);
+        let fs = getFirestore(fbApp);
+        let d = doc(fs, 'users' + userId);
         updateDoc(d, data).then(res => resolve(res)).catch(err => reject(err));
     })
 }
 
 exports.getProduct = (pId) => {
     return new Promise((resolve, reject) => {
-        let d = doc(fbApp, '/products/' + pId);
+        let fs = getFirestore(fbApp);
+        let d = doc(fs, 'products', pId);
         getDoc(d).then(res => resolve(res.data())).catch(err => reject(err));
     });
 }
 
 exports.createTransaction = (tId, data) => {
     return new Promise((resolve, reject) => {
-        let col = collection(fbApp,'/transactions');
-        addDoc(col, data).then(res => resolve(res.id)).catch(err => reject(err));
+        let fs = getFirestore(fbApp)
+        setDoc(doc(fs, 'transactions', tId), data).then(res => resolve(res)).catch(err => reject(err));
     });
 };
 
 exports.updateTransaction = (tId, data) => {
     return new Promise((resolve, reject) => {
-        let d = doc(fbApp, '/transactions/' + tId);
+        let fs = getFirestore(fbApp);
+        let d = doc(fs, 'transactions', tId);
         updateDoc(d, data).then(res => resolve(true)).catch(err => reject(err));
     });
 }
 
 exports.getTransaction = (tId) => {
     return new Promise((resolve, reject) => {
-        let d = doc(fbApp, '/transactions/' + tId);
+        let fs = getFirestore(fbApp);
+        let d = doc(fs, 'transactions', tId);
         getDoc(d).then(res => resolve(res.data())).catch(err => reject(err));
     })
 }
